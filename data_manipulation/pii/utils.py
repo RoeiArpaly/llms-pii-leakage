710474410547bc634356488e57748984175fc1c9
Roei Arpaly <roei.arpaly@gmail.com>
2025-04-13 00:55:10 +0300

    fixing fuzzy_pii_injection

--- data_manipulation/pii/utils.py
+++ data_manipulation/pii/utils.py
@@ -4,29 +4,36 @@ from copy import deepcopy
 
 
 def fuzzy_pii_injection(
-        text: str,
-        spans: list,
-        fuzzy_func: callable,
-        fuzzy_func_kwargs: dict = None,
-        proba: float = 1.0,
-        update_spans: bool = True,
+    text: str,
+    spans: list,
+    fuzzy_func: callable,
+    fuzzy_func_kwargs: dict = None,
+    proba: float = 1.0,
+    update_spans: bool = True,
 ) -> tuple:
-    """Separate the text into components based on the spans."""
-    components = []
-    new_spans = deepcopy(spans)
-    start = 0
-    for i, span in enumerate(spans):
-        end = span["start"]
-        components.append(text[start:end])
-        pii = text[span["start"]:span["end"]]
+    if not spans:
+        return text, []
+    fuzzy_func_kwargs = fuzzy_func_kwargs or {}
+    result = []
+    new_spans = []
+    input_idx = output_idx = 0
+    for span in spans:
+        start, end = span["start"], span["end"]
+        result.append(text[input_idx:start])  # Add unchanged prefix
+        output_idx += start - input_idx
+        pii = text[start:end + 1]
         if random.random() < proba:
-            fuzzy_pii = fuzzy_func(pii, **(fuzzy_func_kwargs or {}))
-            components.append(fuzzy_pii)
-            if update_spans:
-                new_spans[i]["value_fuzzy"] = fuzzy_pii
-                new_spans[i]["end"] = new_spans[i]["start"] + len(fuzzy_pii)
-        else:
-            components.append(pii)
-        start = span["end"]
-    components.append(text[start:])
-    return "".join(components), new_spans
+            pii = fuzzy_func(pii, **fuzzy_func_kwargs)
+        result.append(pii)
+        new_span = deepcopy(span)
+        if update_spans:
+            new_span.update({
+                "start": output_idx,
+                "end": output_idx + len(pii) - 1,
+                "value_fuzzy": pii,
+            })
+        new_spans.append(new_span)
+        input_idx = end + 1
+        output_idx += len(pii)
+    result.append(text[input_idx:])
+    return "".join(result), new_spans
