9284e39e123e8e3ec525bc6369b5405b25dd5399
Roei Arpaly <roei.arpaly@gmail.com>
2025-04-18 23:21:48 +0300

    adding char_to_word

--- data_manipulation/pii/__init__.py
+++ data_manipulation/pii/__init__.py
@@ -1,7 +1,6 @@
+from .char_to_word import char_to_word  # noqa: F401
 from .emojify import emojify_pii  # noqa: F401
 from .homoglyph import homoglyph  # noqa: F401
-from .number_to_word import number_to_word  # noqa: F401
 from .reverse import reverse_pii  # noqa: F401
 from .separators import inject_separator  # noqa: F401
-from .word_symbols import word_symbols  # noqa: F401
 from .chunking import chunking  # noqa: F401
